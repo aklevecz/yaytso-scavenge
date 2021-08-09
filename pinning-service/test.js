b7ec98d990e1d8e8b07042620578ce12e4dcdc51
traysimay <teh@raptor.pizza>
2021-08-08 20:31:34 -0700

    pin

--- pinning-service/test.js
+++ pinning-service/test.js
@@ -2,4 +2,9 @@ require("dotenv").config();
 const { NFTStorage, File, Blob } = require("nft.storage");
 const token = process.env.NFT_STORAGE_TOKEN;
 const client = new NFTStorage({ token });
-console.log(token);
+client
+  .storeDirectory([
+    new File(["hello world"], "hello.txt"),
+    new File([JSON.stringify({ from: "incognito" }, null, 2)], "metadata.json"),
+  ])
+  .then(console.log);
